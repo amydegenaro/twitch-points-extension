e3fe740d1b3f83c9eef26bfa47d6d036cfbd8fb2
Amy De Genaro <amy.degenaro@gmail.com>
2020-01-04 14:56:37 -0500

    get button by tag name

--- background.js
+++ background.js
@@ -15,9 +15,6 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.tabs.onUpdated.addListener(function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    // chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    //   console.log(response.farewell);
-    // });
     chrome.tabs.sendMessage(tabs[0].id, { action: "clickBonusBox" }, function(response) {
       console.log(response.clicked);
     });
--- content.js
+++ content.js
@@ -1,9 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log('HELLOOOOO I AM LISTENING')
 
-  // if (request.greeting == "hello")
-  //   sendResponse({farewell: "goodbye"});
-
   if (request.action === "clickBonusBox") {
     const MutationObserver = window.MutationObserver || window.WebKitMutationObserver
     const targetNode = document.getElementsByClassName("community-points-summary")[0];
@@ -13,11 +10,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       subtree: true
     }
 
+    targetNode.getElementsByTagName('button')[1].click();
+
     const observer = new MutationObserver((mutations) => {
       // what we want to fire when mutation on target node is found
       mutations.forEach(mutation => {
         if (mutation.type === 'childList') {
-          const bonusButton = mutation.target.querySelector('tw-button--success') || document.getElementsByClassName('tw-button--success')[0];
+          const bonusButton = mutation.target.getElementsByTagName('button')[1]
+          || targetNode.getElementsByTagName('button')[1];
+
           if (bonusButton) {
             console.log('Bonus available, clicking...');
             bonusButton.click();
